d9040756e61b3681a37e5e0e3c05b9ddb5a96588
ajit57 <ajitdas2900@gmail.com>
2018-01-15 23:04:19 +0600

    broadcasting correctly

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -27,7 +27,7 @@ class ChatController extends Controller
 
     public function send(Request $request)
     {
-    	return $request->all();
+    	//return $request->all();
     	$user = User::find(Auth::id());
     	event(new ChatEvent($request->message,$user));
     }
